09c45afb428cd563169d9dbdc5d30a7dce017801
Doncho Minkov <DonchoMinkov@gmail.com>
2017-04-24 13:02:32 +0300

    Fixed script do install MySQL and setup db and privileges

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y python-dev supervisor nginx git gcc g++ make python-dev libxml2-dev libxslt1-dev zlib1g-dev ruby-sass gettext curl
+# apt-get install -y python-dev supervisor nginx git gcc g++ make python-dev libxml2-dev libxslt1-dev zlib1g-dev ruby-sass gettext curl
 
-wget -q --no-check-certificate -O- https://bootstrap.pypa.io/get-pip.py | sudo python
-wget -O- https://deb.nodesource.com/setup_7.x | sudo -E bash -
-apt-get update
-apt-get -y install nodejs
-dpkg --configure -a
+# wget -q --no-check-certificate -O- https://bootstrap.pypa.io/get-pip.py | sudo python
+# wget -O- https://deb.nodesource.com/setup_7.x | sudo -E bash -
+# apt-get update
+# apt-get -y install nodejs
+# dpkg --configure -a
 
-npm install -g pleeease-cli
+# npm install -g pleeease-cli
 
 # MySQL setup for development purposes ONLY
 echo -e "\n--- Install MySQL specific packages and settings ---\n"
@@ -18,32 +18,30 @@ debconf-set-selections <<< "mysql-server mysql-server/root_password_again passwo
 apt-get -y install mysql-server  libmysqlclient-dev
 
 echo -e "\n--- Setting up our MySQL user and db ---\n"
-apt-get -y install mysql-server  libmysqlclient-dev
-
-mysql -uvagrant -pvagrant -e "CREATE DATABASE dmoj" 
-mysql -uvagrant -pvagrant -e "grant all privileges on dmoj.* to 'vagrant'@'localhost' identified by 'vagrant'" 
+mysql -uroot -pvagrant -e "CREATE DATABASE dmoj" 
+mysql -uroot -pvagrant -e "grant all privileges on dmoj.* to 'vagrant'@'localhost' identified by 'vagrant'" 
 
 sudo /etc/init.d/mysql restart
 
-wget -q --no-check-certificate -O- https://bootstrap.pypa.io/get-pip.py | sudo python
+# wget -q --no-check-certificate -O- https://bootstrap.pypa.io/get-pip.py | sudo python
 
-pip install virtualenv
+# pip install virtualenv
 
-mkdir -p /envs/dmoj
-virtualenv -p python /envs/dmoj
+# mkdir -p /envs/dmoj
+# virtualenv -p python /envs/dmoj
 
-source /envs/dmoj/bin/activate
+# source /envs/dmoj/bin/activate
 
 if ! [ -L /var/www ]; then
     rm -rf /var/www
     ln -fs /vagrant /var/www
 fi
 
-mkdir -p /vagrant/dmoj
+# mkdir -p /vagrant/dmoj
 
-git clone https://github.com/Minkov/site.git /vagrant/dmoj/site
-cd /vagrant/dmoj/site
-git submodule init
-git submodule update
-cd -
+# git clone https://github.com/Minkov/site.git /vagrant/dmoj/site
+# cd /vagrant/dmoj/site
+# git submodule init
+# git submodule update
+# cd -
 

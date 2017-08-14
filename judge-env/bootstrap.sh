f43d5fefab736220d3b56839e3b028719c14e023
Doncho Minkov <DonchoMinkov@gmail.com>
2017-08-14 17:27:31 +0300

    Added more languages

--- judge-env/bootstrap.sh
+++ judge-env/bootstrap.sh
@@ -2,7 +2,11 @@
 
 apt-get update
 apt-get upgrade
-apt-get install -y sudo apt git python-dev python-pip build-essential mono-complete openjdk-8-jdk
+apt-get install -y sudo apt git python-dev python-pip build-essential mono-complete openjdk-8-jdk ruby php ghc \
+    clang libicu-dev 
+
+wget -q -O - https://swift.org/keys/all-keys.asc | \
+  gpg --import -
 
 pip install --upgrade pip
 
@@ -21,7 +25,7 @@ git clone https://github.com/cuklev/dsa-miniexam-tasks-dmoj.git /problems
 cp /vagrant/systemd_files/* /etc/systemd/system/
 
 for cmd in enable start; do
-	for unit in dmoj-judge@WorkPc{1..3}.service dmoj-sync-problems.timer; do
+	for unit in dmoj-judge@WorkPcDoncho{1..3}.service dmoj-sync-problems.timer; do
 		systemctl $cmd $unit
 	done
 done

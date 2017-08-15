785139f01d8f8c89edbc0d1bc779f949f92788f6
Doncho Minkov <DonchoMinkov@gmail.com>
2017-08-15 18:20:18 +0300

    Merge

--- judge-env/bootstrap.sh
+++ judge-env/bootstrap.sh
@@ -5,14 +5,10 @@ apt-get upgrade
 apt-get install -y sudo apt git python-dev python-pip build-essential mono-complete openjdk-8-jdk ruby php ghc \
     clang libicu-dev 
 
-wget -q -O - https://swift.org/keys/all-keys.asc | \
-  gpg --import -
-
 pip install --upgrade pip
 
 pip install setuptools
 pip install cython
-
 
 # Swift
 
@@ -24,6 +20,7 @@ sudo chown nobody /opt/swift4/usr/lib/swift/CoreFoundation/module.modulemap
 cd /vagrant
 git clone https://github.com/minkov/judge /vagrant/judge
 cd judge
+
 pip install -r requirements.txt
 
 python setup.py develop
@@ -33,7 +30,8 @@ git clone https://github.com/cuklev/dsa-miniexam-tasks-dmoj.git /problems
 cp /vagrant/systemd_files/* /etc/systemd/system/
 
 for cmd in enable start; do
-	for unit in dmoj-judge@WorkPcDoncho{1..3}.service dmoj-sync-problems.timer; do
+    for unit in dmoj-judge@AwsJudge{1..3}.service dmoj-sync-problems.timer; do
+
 		systemctl $cmd $unit
 	done
 done

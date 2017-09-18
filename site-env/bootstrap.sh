2ab6916b4f59c1402ec4bd2eff64667b6529eaaf
Kristiyan Tsaklev <cuki@cukii.tk>
2017-09-18 16:39:39 +0300

    Create dmoj DB with unicode collate

--- site-env/bootstrap.sh
+++ site-env/bootstrap.sh
@@ -49,7 +49,7 @@ echo -e "\n --- Installing and Setting up MySQL ---\n"
 	apt-get -y install mysql-server libmysqlclient-dev
 
 # echo -e "\n--- Setting up our MySQL user and db ---\n"
-	mysql -uroot -pvagrant -e "CREATE DATABASE dmoj"
+	mysql -uroot -pvagrant -e "CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci"
 	mysql -uroot -pvagrant -e "grant all privileges on dmoj.* to 'vagrant'@'localhost' identified by 'vagrant'"
 
 	systemctl restart mysql
